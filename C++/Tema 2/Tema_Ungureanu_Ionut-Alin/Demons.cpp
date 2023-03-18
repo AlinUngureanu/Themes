9535d54cd3bd8e8ab09b9ebbf04fa2db7c145dcc
AlinUngureanu <128098052+AlinUngureanu@users.noreply.github.com>
2023-03-18 20:17:06 +0200

    Delete C++ directory

--- C++/Tema 2/Tema_Ungureanu_Ionut-Alin/Demons.cpp
+++
@@ -1,9 +0,0 @@
-#include "Demons.h"
-
-void Demons::urare() {
-	std::cout << "Salutare!";
-}
-
-void Demons::prezentare() {
-	std::cout << "Eu sunt " << name;
-}
--- C++/Tema 2/Tema_Ungureanu_Ionut-Alin/Player.cpp
+++
@@ -1,9 +0,0 @@
-#include "Player.h"
-
-void Player::urare() {
-	std::cout << "Bine ati venit la joc!";
-}
-
-void Player::prezentare() {
-	std::cout << "Aceasta este o lupta!";
-}
