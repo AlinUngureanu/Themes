71868d7cb2ff7c3047e9bf1d1791c05e1ea00a6f
AlinUngureanu <128098052+AlinUngureanu@users.noreply.github.com>
2023-04-14 16:15:59 +0300

    Add files via upload

---
+++ C#/Quiz_application/Form_Login.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Tema1
+{
+    public partial class Form_Login : Form
+    {
+        public Form_Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button_login_Click(object sender, EventArgs e)
+        {
+            StreamReader streamReader = new StreamReader(path: "login.txt");
+            {
+                string line = string.Empty;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    string[] loginArray = line.Split(' ');
+                    if (textBox_user.Text.Equals(loginArray[0]) && 
+                        textBox_password.Text.Equals(loginArray[1]))
+                    {
+                        Form_ChooseDomain f1 = new Form_ChooseDomain(loginArray[0]);
+                        f1.Show();
+                    }
+                    else
+                    {
+                        if (textBox_user.Text.Equals("") && textBox_password.Text.Equals("") ||
+                           textBox_user.Text.Equals("") || textBox_password.Text.Equals(""))
+                        {
+                            MessageBox.Show("Nu ați introdus username/parola sau ambele.", "Eroare");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Ați introdus userul/parola greșit!" +
+                            Environment.NewLine + "Mai încercați!", "Eroare");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ C#/Quiz_application/Form_Questions.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace Tema1
+{
+    partial class Form_Questions
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_next = new System.Windows.Forms.Button();
+            this.label_title = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.button_exit = new System.Windows.Forms.Button();
+            this.label_percent = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button_next
+            // 
+            this.button_next.Location = new System.Drawing.Point(327, 304);
+            this.button_next.Name = "button_next";
+            this.button_next.Size = new System.Drawing.Size(75, 26);
+            this.button_next.TabIndex = 5;
+            this.button_next.Text = "Next";
+            this.button_next.UseVisualStyleBackColor = true;
+            this.button_next.Click += new System.EventHandler(this.button_next_Click);
+            // 
+            // label_title
+            // 
+            this.label_title.AutoSize = true;
+            this.label_title.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_title.Location = new System.Drawing.Point(300, 30);
+            this.label_title.Name = "label_title";
+            this.label_title.Size = new System.Drawing.Size(144, 18);
+            this.label_title.TabIndex = 6;
+            this.label_title.Text = "Întrebări din domeniu";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(557, 183);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(113, 106);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(310, 156);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Întrebare";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(21, 118);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(97, 21);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.Text = "Răspuns 4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(21, 91);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(97, 21);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.Text = "Răspuns 3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(21, 64);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(97, 21);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "Răspuns 2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(21, 37);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(97, 21);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.Text = "Răspuns 1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // button_exit
+            // 
+            this.button_exit.Location = new System.Drawing.Point(327, 371);
+            this.button_exit.Name = "button_exit";
+            this.button_exit.Size = new System.Drawing.Size(75, 26);
+            this.button_exit.TabIndex = 9;
+            this.button_exit.Text = "Exit";
+            this.button_exit.UseVisualStyleBackColor = true;
+            this.button_exit.Click += new System.EventHandler(this.button_exit_Click);
+            // 
+            // label_percent
+            // 
+            this.label_percent.AutoSize = true;
+            this.label_percent.Location = new System.Drawing.Point(589, 209);
+            this.label_percent.Name = "label_percent";
+            this.label_percent.Size = new System.Drawing.Size(68, 17);
+            this.label_percent.TabIndex = 10;
+            this.label_percent.Text = "Procentaj";
+            // 
+            // Form_Questions
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label_percent);
+            this.Controls.Add(this.button_exit);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label_title);
+            this.Controls.Add(this.button_next);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form_Questions";
+            this.Text = "Form_Questions";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button_next;
+        private System.Windows.Forms.Label label_title;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button_exit;
+        private System.Windows.Forms.Label label_percent;
+    }
+}
